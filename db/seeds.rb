294d21f112e7f5ceb32a1d24e9e98b082dedb674
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-08-10 21:50:53 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bristol = Restaurant.new(name: "Epicure", category: "french", address: "qohvsfivjivje")
+bristol.save
+
+michel = Restaurant.new(name: "michel", category: "chinese", address: "qohvsfivjivje")
+michel.save
+
+guy = Restaurant.new(name: "guy", category: "japanese", address: "qohvsfivjivje")
+guy.save
+
+bastien = Restaurant.new(name: "bastien", category: "belgian", address: "qohvsfivjivje")
+bastien.save
+
+parma = Restaurant.new(name: "parma", category: "italian", address: "qohvsfivjivje")
+parma.save
